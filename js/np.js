1f2eaf4ff2c9c536b7a8b4eef877a4c1e294b57e
Kitty Hanahara <KittyHanahara@kittymail.com>
2019-08-07 16:12:26 -0400

    Deleted files, and made cool animation happen at start up

--- js/np.js
+++ js/np.js
@@ -2,7 +2,6 @@ var links = document.getElementsByClassName('copyjs');
 
 Array.from(links).forEach(element => {
   var clipboard = new ClipboardJS(element);
-  console.log(element);
   element.onclick = function(e) {
     e.preventDefault();
     var orig = element.innerHTML;
@@ -12,3 +11,44 @@ Array.from(links).forEach(element => {
     }, 2000);
   };
 });
+
+window.onload = function() {
+  intro();
+}
+
+function intro() {
+
+  var cover1 = document.getElementById("cover1");
+  var cover2 = document.getElementById("cover2");
+  var cover3 = document.getElementById("cover3");
+
+  TweenLite.to(cover1, 0.2, {
+    ease: Power2.easeInOut,
+    left: "-250%",
+  });
+  TweenLite.to(cover2, 0.2, {
+    delay: 0.2,
+    ease: Power2.easeInOut,
+    left: "150%",
+  });
+  TweenLite.to(cover3, 0.2, {
+    delay: 0.4,
+    ease: Power2.easeInOut,
+    left: "-250%",
+    onComplete: presentLogo
+  });
+  TweenLite.to(cover4, 0.2, {
+    delay: 0.6,
+    ease: Power2.easeInOut,
+    left: "150%",
+  });
+}
+
+function presentLogo() {
+  var logo = document.getElementsByClassName("logo");
+  TweenLite.from(logo[0], 0.8, {
+    ease: Power2.easeOut,
+    scaleX: 1.5,
+    scaleY: 1.5,
+  })
+}
